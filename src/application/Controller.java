c03cef59c046346da24c9aa2b022da1a27c83ded
STAS <stas.elovsky@gmail.com>
2019-12-04 18:29:02 +0300

    initial commit

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,4 +1,56 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import java.awt.*;
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    TextField pValue;
+
+    @FXML
+    TextField qValue;
+
+    @FXML
+    TextField hValue;
+
+    @FXML
+    TextField xValue;
+
+    @FXML
+    TextField kValue;
+
+    @FXML
+    TextField rValue;
+
+    @FXML
+    TextField sValue;
+
+    @FXML
+    TextField gValue;
+
+    @FXML
+    TextField yValue;
+
+    @FXML
+    TextField hashValue;
+
+    @FXML
+    Text filePathText;
+
+    private String filePath;
+
+    @FXML
+    void choseFile() {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(filePathText.getScene().getWindow());
+        if (file != null) {
+            filePath = file.getPath();
+            filePathText.setText(file.getName());
+        }
+    }
 }
